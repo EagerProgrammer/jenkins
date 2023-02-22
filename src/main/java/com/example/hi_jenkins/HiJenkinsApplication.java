5273bf610cf1b9e8c846a6dae682dee204b7f704
EagerProgrammer <yth960617@unist.ac.kr>
2023-02-22 09:39:41 +0900

    jenkins

---
+++ src/main/java/com/example/hi_jenkins/HiJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hi_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/hi_jenkins/Jenkins.java
@@ -0,0 +1,13 @@
+package com.example.hi_jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Jenkins {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello jenkins";
+    }
+}
